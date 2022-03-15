47cee9b5ee83fd902c967fd50d5acc98703efced
Emalison <39349876+Emalison@users.noreply.github.com>
2022-03-16 01:11:43 +0300

    Update combine.py

--- combine.py
+++ combine.py
@@ -1,41 +1,82 @@
-import threading
+from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.common.desired_capabilities import DesiredCapabilities
+from selenium.common.exceptions import TimeoutException
+import random
+from selenium import webdriver
+from selenium.webdriver.common.action_chains import ActionChains
 import time
+from selenium.common.exceptions import NoSuchElementException
+
+from selenium.webdriver.firefox.options import Options
+from selenium.webdriver.chrome.service import Service
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.common.action_chains import ActionChains
+import os
+
+from selenium.webdriver.common.by import By
+
+from selenium.webdriver.support.select import Select
+import random
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+import datetime
+import os
+from selenium import webdriver
+import time
+import random
+import smtplib
+
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.firefox.firefox_binary import FirefoxBinary
 
 
-def mon11():
-    import mon1
+def load_driver():
+    options = webdriver.FirefoxOptions()
 
-def mon21():
-    import mon2
+    # enable trace level for debugging
+    options.log.level = "trace"
 
-def mon31():
-    import mon3
+    options.add_argument("-remote-debugging-port=9224")
+    options.add_argument("-headless")
+    options.add_argument("-disable-gpu")
+    options.add_argument("-no-sandbox")
 
+    binary = FirefoxBinary(os.environ.get('FIREFOX_BIN'))
 
+    firefox_driver = webdriver.Firefox(
+        firefox_binary=binary,
+        executable_path=os.environ.get('GECKODRIVER_PATH'),
+        options=options)
 
-
-
-x = threading.Thread(target=mon11, args=())
-x.start()
-
-w = threading.Thread(target=mon21, args=())
-w.start()
-
-y = threading.Thread(target=mon31, args=())
-y.start()
+    return firefox_driver
 
 
+driver = load_driver()
+driver.get("https://app.growviews.com/#/login")
+time.sleep(10)
+scratch = driver.find_element_by_xpath("/html/body/div[3]/div/div/section/div/div[2]/form/div[1]/input")
+scratch.send_keys("spidombaya@yandex.com")
+print("jkikk")
+scratch = driver.find_element_by_xpath("/html/body/div[3]/div/div/section/div/div[2]/form/div[2]/input")
+scratch.send_keys("spido900")
+time.sleep(3)
+print("hjjj")
+driver.find_element_by_xpath("/html/body/div[3]/div/div/section/div/div[2]/form/div[3]/div/button").click()
+time.sleep(6)
+driver.get("https://app.growviews.com/#/earn/auto")
+time.sleep(8)
+print("okokjjj")
+driver.find_element_by_xpath("/html/body/div[3]/div/div[1]/div/div/div[1]/div/div[2]/div/div/div[1]/button").click()
+time.sleep(6000)
+driver.get("https://app.growviews.com/#/videos/add-video")
+time.sleep(10)
+scratch77 = driver.find_element_by_xpath("/html/body/div[1]/div[1]/div/div/div[2]/form/div[2]/input")
+scratch77.send_keys("https://www.youtube.com/watch?v=vugL3C6uO7w")
+time.sleep(5)
+driver.find_element_by_xpath("/html/body/div[1]/div[1]/div/div/form/button[2]").click()
+time.sleep(10)
+driver.find_element_by_xpath("/html/body/div[1]/div[1]/div/div/div[2]/div/div/div[1]/div/div[2]/div[2]/div/div[2]/span[2]/button").click()
+time.sleep(6)
+driver.quit()
 
-
-
-
-w.join()
-x.join()
-y.join()
-
-
-
-
-
-
-print(time.perf_counter())
